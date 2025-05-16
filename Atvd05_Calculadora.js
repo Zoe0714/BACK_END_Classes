bfaa28485bf66b1e752f187cc860a275d854768d
Zoe0714 <159541965+Zoe0714@users.noreply.github.com>
2025-05-16 09:08:45 -0300

    Add files via upload

---
+++ Atvd05_Calculadora.js
@@ -0,0 +1,15 @@
+class Calculadora{
+    somar(a,b){
+        return a + b;
+    }
+    subtrair(a,b){
+        return a - b;
+    }
+    multiplicar(a,b){
+        return a * b;
+    }
+    dividir(a,b){
+        return a / b;
+    }
+    console.log(`${}`)
+}
